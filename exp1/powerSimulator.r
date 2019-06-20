b547baf1f9615af8882e2ba5b08da82d9537b28e
Dave <dab414@lehigh.edu>
2019-06-20 12:04:34 -0400

    Simulating one experiment dataset seems to be working well

--- exp1/powerSimulator.r
+++ exp1/powerSimulator.r
@@ -3,25 +3,31 @@ nTrials <- 700
 
 ## FIXED EFFECTS
 exponentsFixed <- .3
-lossAversionFixed <- 2
-fixedEffects <- c('exponentsFixed' = exponentsFixed, 'lossAversionFixed' = lossAversionFixed)
+lossAversionFixed <- 3
+learningFixed <- 200
 
 ## RANDOM EFFECTS
 ## keeping these pretty tight for now
-# interceptRandom <- abs(toLogOdds(.1))
-# exponentsRandom <- .2
-# lossAversionRandom <- 1
+#interceptRandom <- abs(toLogOdds(.05))
+exponentsRandom <- .2
+lossAversionRandom <- 1
+learningRandom <- 150
 
-interceptRandom <- 0
-exponentsRandom <- 0
-lossAversionRandom <- 0
+ interceptRandom <- 0
+# exponentsRandom <- 0
+# lossAversionRandom <- 0
+
 
 ## within-subject noise
 noiseSd <- 0
+withinBlockLearningRateNoise <- 50
   
 buildSubjectProfile <- function() {
   ## Returns a vector with values to be added to / subtracted from fixed effects for each subject
-  subjectProfile <- c(subjectIntercept = rnorm(1, 0, interceptRandom), subjectExponents = rnorm(1, 0, exponentsRandom), subjectLossAversion = rnorm(1, 0, lossAversionRandom))
+  subjectProfile <- c(subjectIntercept = rnorm(1, 0, interceptRandom), 
+                      subjectExponents = rnorm(1, 0, exponentsRandom), 
+                      subjectLossAversion = rnorm(1, 0, lossAversionRandom), 
+                      subjectLearning = rnorm(1, 0, learningRandom))
   return(subjectProfile)
 }
   
@@ -42,26 +48,136 @@ valueFunction <- function(criticalDeckIntensity, bias, subjectProfile) {
   return(out)
 }
   
-decision <- function(criticalDeckIntensity, bias, trial, trialMax, subjectProfile) {
+decision <- function(criticalDeckIntensity, bias, trial, subjectProfile, learningRate) {
   ## takes as input all this stuff above
   ## returns the probability of choosing the reference deck
   
-
-  
   ## Level 1 equation
-  proba <- subjectProfile['subjectIntercept'] + valueFunction(criticalDeckIntensity, bias, subjectProfile) * (trial / trialMax) + rnorm(1, 0, noiseSd) 
+  proba <- subjectProfile['subjectIntercept'] + valueFunction(criticalDeckIntensity, bias, subjectProfile) * min((trial / learningRate), 1) + rnorm(1, 0, noiseSd) 
   proba <- exp(proba) / (1 + exp(proba))
   
   return(proba)
   
 }
   
+powerSimulator <- function(n, bias, nSims) {
+  ## function takes in:
+    ## n: sample size
+    ## bias: functional effect size
+    ## nSims: number of simulations
+  ## returns:
+    ## one row of a df with n, POWER, effect size, nSims, and power outcome
+  
+  ## if only 1 sim, return the experiment data instead
+  oneShot <- ifelse(nSims == 1, 1, 0)
+  
+  experimentResults <- data.frame(n = numeric(), bias = numeric(), effectSize = numeric(), pValue = numeric(), predictedDirection = logical())
+  write.csv(experimentResults, 'experimentResultsCache.csv', row.names = FALSE)
+  
+  ## Sim loop
+  for (sim in 1:nSims) {
+    experimentData <- data.frame(subject = numeric(), trial = numeric(), difference = factor(, levels = c('moderate', 'extreme')), 
+                                 difficulty = factor(,levels = c('easier', 'harder')), referenceSelection = numeric())
+    write.csv(experimentData, 'experimentDataCache.csv', row.names = FALSE)
+    print(paste('Simulation', sim, 'of', nSims))
+    
+    ## Subject loop
+    for (subject in 1:n) {
+      print(paste('subject:', subject))
+      subjectProfile <- buildSubjectProfile()
+      
+      ## condition codes
+      conditions <- data.frame(difficulty = factor(c(rep('easier', 2), rep('harder', 2))), 
+                               difference = factor(rep(c('moderate', 'extreme'), 2)),
+                               criticalDeckIntensity = c(0.3, 0.1, 0.7, 0.9))
+   
+      ## block loop
+      for (conditionRow in 1:4) {
+        ## true trial count
+        count <- 0
+        ## blocked trial count
+        trialIterator <- 1:(nTrials/4)
+        
+        ## create learning rate with min trials to learning being 50
+        ## also between-block noise in learning rate with SD 25
+        learningRate <- max(round(learningFixed + subjectProfile['subjectLearning'] + rnorm(1, 0, withinBlockLearningRateNoise)), 25)
+        
+        for (trial in trialIterator) {
+          count <- count + 1
+          
+          ## compute decision threshold
+          proba <- decision(criticalDeckIntensity = conditions[conditionRow,]$criticalDeckIntensity, 
+                            bias = bias, 
+                            trial = trial, 
+                            subjectProfile = subjectProfile,
+                            learningRate = learningRate)
+          
+          ## execute decision and save data
+          experimentData <- rbind(experimentData, data.frame(subject = subject, trial = count,
+                                                             difference = conditions[conditionRow,]$difference,
+                                                             difficulty = conditions[conditionRow,]$difficulty,
+                                                             referenceSelection = ifelse(runif(1) < proba, 1, 0)))
+          
+          
+        } ## end trial iterator
+      } ## end block loop
+      
+      ## every 10 subjects, save out data to cache and dump
+      if (subject %% 5 == 0 | subject == n) {
+        experimentData <- rbind(read.csv('experimentDataCache.csv', header = TRUE), experimentData)
+        write.csv(experimentData, 'experimentDataCache.csv', row.names = FALSE)
+        print(paste('Subject: ', subject, ' of ', n, sep = ''))
+        if (subject != n) {
+          experimentData <- data.frame(subject = numeric(), trial = numeric(), difference = factor(, levels = c('moderate', 'extreme')), 
+                                       difficulty = factor(,levels = c('easier', 'harder')), referenceSelection = numeric())
+        }
+      }
+      
+    } ## end subject loop
+    
+    ## compile results from simulation
+    #experimentResults <- computeStats(translateToDeviation(experimentData), n, bias)
+    
+    
+  } ## end simLoop()
+  
+  return(experimentData)
+  
+} ## end powerSimulator()
   
   
   
+translateToDeviation <- function(d) {
+  ## takes as input experiment data 
+  d <- d %>% 
+    group_by(subject, difference, difficulty) %>% 
+    summarize(referenceSelection = mean(referenceSelection)) %>% 
+    unite('condition', c('difference', 'difficulty')) %>% 
+    spread(condition, referenceSelection) %>% 
+    mutate(moderate_easier_deviation = abs(moderate_easier - 0.5),
+           moderate_harder_deviation = abs(moderate_harder - 0.5),
+           extreme_easier_deviation = abs(extreme_easier - moderate_easier),
+           extreme_harder_deviation = abs(extreme_harder - moderate_harder)) %>% 
+    select(subject, contains('deviation')) %>% 
+    gather(condition, referenceDeviation, contains('deviation')) %>% 
+    separate(condition, c('difference', 'difficulty'))
   
+  return(d)
   
+}
 
+  
+computeStats <- function(experimentData, n, bias) {
+  ## takes as input the dataset from a single simulation and returns a dataset summarizing parameters from an ANOVA as well as the levels of the hyperparameters
+  experimentData$subject <- factor(experimentData$subject)
+  experimentData$difference <- factor(experimentData$difference)
+  experimentData$difficulty <- factor(experimentData$difficulty)
+
+  m1 <- ezANOVA(wid = subject, within = .(difference, difficulty), dv = referenceDeviation, data = experimentData, detailed = TRUE)
+  
+  
+  
+}
 
 
 
